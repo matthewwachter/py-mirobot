899f6434736e3cf00948ce7647674568ea859a8a
matthewwachter <matthewwachter@gmail.com>
2020-02-22 13:38:41 -0800

    added is_open property

--- serial_device.py
+++ serial_device.py
@@ -11,18 +11,21 @@ class SerialDevice:
         self.listen_callback = None
         self.listen_thread = None
         self.serialport = serial.Serial()
+        self._is_open = False
 
     # close the serial port when the class is deleted
     def __del__(self):
         try:
-            if self.is_open():
+            if self._is_open:
                 self.serialport.close()
         except:
             print("Destructor error closing COM port: ", sys.exc_info()[0] )
 
     # check if the serial port is open
+    @property
     def is_open(self):
-        return self.serialport.is_open()
+        self._is_open = self.serialport.is_open()
+        return self._is_open
 
     # sets the listen callback
     def set_listen_callback(self, listen_callback):
@@ -31,7 +34,7 @@ class SerialDevice:
 
         self.listen_callback = listen_callback
 
-        if self.is_open():
+        if self._is_open:
             self.start_listen_thread()
 
     # starts the listen thread
@@ -43,7 +46,7 @@ class SerialDevice:
     def listen_to_device(self, listen_callback):
         while True:
             try:
-                if self.is_open():
+                if self._is_open:
                     msg = self.serialport.readline()
                     if msg != "":
                         if listen_callback is not None:
@@ -53,30 +56,30 @@ class SerialDevice:
 
     # open the serial port
     def open(self):
-        if not self.is_open():
+        if not self._is_open:
             # serialport = 'portname', baudrate, bytesize = 8, parity = 'N', stopbits = 1, timeout = None, xonxoff = 0, rtscts = 0)
             self.serialport.port = self.portname
             self.serialport.baudrate = self.baudrate
             self.serialport.stopbits = self.stopbits
             try:
                 self.serialport.open()
-                self.isopen = True
+                self._is_open = True
                 self.start_listen_thread()
             except:
                 print("Error opening COM port: ", sys.exc_info()[0])
 
     # close the serial port
     def close(self):
-        if self.is_open():
+        if self._is_open:
             try:
                 self.serialport.close()
-                self.isopen = False
+                self._is_open = False
             except:
                 print("Close error closing COM port: ", sys.exc_info()[0])
 
     # send a message to the serial port
     def send(self, message, terminator='\r\n'):
-        if self.is_open():
+        if self._is_open:
             try:
                 message += terminator
                 self.serialport.write(message.encode('utf-8'))
