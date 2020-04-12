10be21d1c0cf9a8e475b49a579fe7ede1d46c23b
Blake Hannaford <blake@uw.edu>
2020-04-12 13:37:06 -0700

    moving work to a fork vs a clone

---
+++ home_quit.py
@@ -0,0 +1,36 @@
+from time import sleep
+import sys
+from .mirobot import Mirobot
+
+
+def msg_cb(message):
+    if not 'ok' in message.lower():
+        print('>>'+message)
+    #print('CB: ', message)
+
+    #Interesting = False
+    #if '<Idle,' in message:
+        #Interesting = True
+    #if Interesting:
+        ##pass
+        #print('CB: ', message)
+
+def wait(msg):
+    print(' waiting for ',msg,'... ', flush=True)
+    x = input('continue...')
+    #sleep(15)
+
+m = Mirobot(debug=False)
+portname = '/dev/ttyUSB0'
+m.connect(portname)
+#m.set_receive_callback(msg_cb)
+
+delaytime = 1
+i = 0
+sleep(2)
+m.home_simultaneous()
+
+wait('homing')
+
+quit()
+ 
